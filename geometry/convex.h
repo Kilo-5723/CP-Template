0ed8185710550f45772daac98cf7cc584b40a72c
Kilo_5723 <yunxiang.zhao.stu@gmail.com>
2023-08-21 04:44:24 +0800

    upd

--- geometry/convex.h
+++ geometry/convex.h
@@ -1,12 +1,54 @@
-
+typedef long long ll;
+typedef long double ld;
 struct cplx {
   ll x, y;
-  double abs() { return sqrt(x * x + y * y); }
+  ll abs2() { return x * x + y * y; }
+  ld abs() { return sqrt(abs2()); }
 };
-cplx operator+(cplx a, cplx b) { return {a.x + b.x, a.y + b.y}; }
-cplx operator-(cplx a, cplx b) { return {a.x - b.x, a.y - b.y}; }
-ll dot(cplx a, cplx b) { return a.x * b.x + a.y * b.y; }
-ll det(cplx a, cplx b) { return a.x * b.y - a.y * b.x; }
+cplx operator+(const cplx &a, const cplx &b) { return {a.x + b.x, a.y + b.y}; }
+cplx operator-(const cplx &a, const cplx &b) { return {a.x - b.x, a.y - b.y}; }
+bool operator<(const cplx &a, const cplx &b) {
+  return a.x != b.x ? a.x < b.x : a.y < b.y;
+}
+bool operator!=(const cplx &a, const cplx &b) { return a < b || b < a; }
+bool operator==(const cplx &a, const cplx &b) { return !(a == b); }
+ll dot(const cplx &a, const cplx &b) { return a.x * b.x + a.y * b.y; }
+ll det(const cplx &a, const cplx &b) { return a.x * b.y - a.y * b.x; }
+void diagsort(vector<cplx> &a, cplx o) {
+  sort(a.begin(), a.end(), [&](const cplx &p, const cplx &q) {
+    if ((o < p) != (o < q)) return o < p;
+    auto d = det(p - o, q - o);
+    if (d) return d > 0;
+    return (p - o).abs2() > (q - o).abs2();
+  });
+  a.resize(unique(a.begin(), a.end(),
+                  [&](const cplx &p, const cplx &q) {
+                    return !det(p - o, q - o) && dot(p - o, q - o) > 0;
+                  }) -
+           a.begin());
+}
+vector<cplx> convex(vector<cplx> a) {
+  sort(a.begin(), a.end());
+  auto o = a[0];
+  a.erase(a.begin());
+  diagsort(a, o);
+  vector<cplx> res = {o};
+  for (auto v : a) {
+    while (res.size() >= 2) {
+      auto p = res[res.size() - 2], q = res[res.size() - 1];
+      if (det(q - p, v - q) > 0) break;
+      res.pop_back();
+    }
+    res.push_back(v);
+  }
+  return res;
+}
+double diameter(const vector<cplx> &a) {
+  double res = (a.back() - a[0]).abs();
+  for (int i = 1; i < a.size(); i++) res += (a[i] - a[i - 1]).abs();
+  return res;
+}
+
 cplx find(vector<cplx> &conv, cplx a) {
   int l = 0, r = conv.size() - 1;
   while (r - l) {
