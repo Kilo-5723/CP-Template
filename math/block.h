035f9467dea4d9124eedb74cba6a2da397aa7850
Kilo-5723 <yunxiang.zhao.stu@gmail.com>
2024-03-23 07:04:06 +0800

    u[d

--- math/block.h
+++ math/block.h
@@ -1,5 +1,7 @@
-for (ll l = 1; l <= n;) {
-  ll r = n / (n / l);
+for (ll l = 1, r; l <= n; l = r + 1) {
+  r = n / (n / l);
   ...  // for l<=i<=r, n/i = n/l
-  l = r + 1;
 }
+/*
+https://qoj.ac/contest/1103/problem/5503
+*/
