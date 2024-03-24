cffb5eb21a93b97b92417fb68a947d30e21dd4e5
Kilo-5723 <yunxiang.zhao.stu@gmail.com>
2024-03-25 00:28:39 +0800

    delaunay updated, pool updated

--- data_structure/pool.h
+++ data_structure/pool.h
@@ -1,11 +1,8 @@
+// ver 2.0
 struct node {
-  int l, r;
-  node *ls, *rs;
-  int tag;
-  node() : l(0), r(0), ls(nullptr), rs(nullptr), tag(0) {}
   static void* operator new(size_t count) {
     static node *begin = nullptr, *end = nullptr;
-    if (begin == end) begin = (node*)malloc(count * 1000), end = begin + 1000;
+    if (begin == end) begin = (node*)malloc(count * 100), end = begin + 100;
     return begin++;
   }
 };
--- geometry/delaunay.h
+++ geometry/delaunay.h
@@ -1,6 +1,4 @@
 // ver 2.0
-int sgn(ll n) { return (n > 0) - (n < 0); }
-int sgn(li n) { return (n > 0) - (n < 0); }
 struct cplx {
   ll x, y;
   ll abs2() const { return x * x + y * y; }
@@ -20,50 +18,66 @@ li det(li a11, li a12, li a13, li a21, li a22, li a23, li a31, li a32, li a33) {
 // check whether o is strictly inside the circle passing a, b, c
 // ref: https://zhuanlan.zhihu.com/p/534117002
 bool inside(cplx a, cplx b, cplx c, cplx o) {
-  if (sgn(det(b - a, c - a)) < 0) swap(b, c);  // guarantee a->c is above a->b
+  if ((det(b - a, c - a)) < 0) swap(b, c);  // guarantee a->c is above a->b
   a = a - o, b = b - o, c = c - o;
   auto d = det(a.x, a.y, a.abs2(), b.x, b.y, b.abs2(), c.x, c.y, c.abs2());
-  return sgn(d) > 0;
+  return d > 0;
 }
-struct edge {
-  int v;
-  list<edge>::iterator rev;
+struct edge {  // cyclic chain with a nil node indicating the head
+  int to;
+  edge *rev;              // rev: reverse edge
+  array<edge *, 2> next;  // next[0]: next next[1]: prev
+  edge(int to) : to(to), rev(nullptr), next({nullptr, nullptr}) {}
+  static void *operator new(size_t count) {
+    static edge *begin = nullptr, *end = nullptr;
+    if (begin == end) begin = (edge *)malloc(count * 100), end = begin + 100;
+    return begin++;
+  }
 };
-// find DT of a[ord[l]]..a[ord[r-1]] stored in e
+pair<edge *, edge *> newedge(int u, int v) {
+  auto uv = new edge(v), vu = new edge(u);
+  uv->rev = vu, vu->rev = uv;
+  return {uv, vu};
+}
+// insert v in direction d of u
+void insert(edge *u, bool d, edge *v) {
+  auto w = u->next[d];  // u -> v -> w
+  u->next[d] = v, v->next[!d] = u;
+  v->next[d] = w, w->next[!d] = v;
+}
+// erase u
+void erase(edge *u) {
+  auto v = u->next[1], w = u->next[0];  // v -> u -> w
+  v->next[0] = w, w->next[1] = v;
+}
+// next non-nil node
+edge *next(edge *u, bool d) {
+  u = u->next[d];
+  return ~u->to ? u : u->next[d];
+}
+// find DT of a[ord[l]]..a[ord[r-1]] stored in h
 // return an arbitrary point (low for speed) of convex a[ord[l]]..a[ord[r-1]]
 // edges are sorted counter-clockwise
 // begin is the first edge on halfplane for vertex on the border of convex hull
-int divconq(const vector<cplx> &a, const vector<int> &ord,
-            vector<list<edge>> &e, int l, int r) {
+int divconq(const vector<cplx> &a, const vector<int> &ord, vector<edge *> &h,
+            int l, int r) {
   if (r - l <= 2) {
     if (r - l == 1) return ord[l];  // return the only point of convex
-    if (r - l == 2) {
-      auto u = ord[l], v = ord[l + 1];
-      auto euv = e[u].insert(e[u].end(), edge()),
-           evu = e[v].insert(e[v].end(), edge());
-      *euv = {v, evu}, *evu = {u, euv};
-      return a[u].y < a[v].y ? u : v;
-    }  // return low point of convex
+    auto u = ord[l], v = ord[l + 1];
+    auto [uv, vu] = newedge(u, v);
+    insert(h[u], 0, uv), insert(h[v], 0, vu);
+    return a[u].y < a[v].y ? u : v;  // return low point of convex
   }
-  int u = divconq(a, ord, e, l, l + (r - l) / 2),  // low point of left convex
-      v = divconq(a, ord, e, l + (r - l) / 2, r),  // low point of right convex
+  int u = divconq(a, ord, h, l, l + (r - l) / 2),  // low point of left convex
+      v = divconq(a, ord, h, l + (r - l) / 2, r),  // low point of right convex
       w = a[u].y < a[v].y ? u : v;                 // low point of convex
   // find neighbour of u lower than line uv (current LR-edge)
-  auto lowerl = [&](int u, int v) -> optional<int> {
-    for (auto [w, r] : e[u]) {
-      auto d = det(a[u] - a[v], a[w] - a[v]);
-      if (sgn(d) > 0 ||
-          sgn(d) == 0 && (a[u] - a[v]).abs2() > (a[w] - a[v]).abs2())
-        return w;
-    }
-    return nullopt;
-  };
-  // find neighbour of v lower than line uv (current LR-edge)
-  auto lowerr = [&](int u, int v) -> optional<int> {
-    for (auto [w, r] : e[v]) {
-      auto d = det(a[v] - a[u], a[w] - a[u]);
-      if (sgn(d) < 0 ||
-          sgn(d) == 0 && (a[v] - a[u]).abs2() > (a[w] - a[u]).abs2())
+  // f = 0: not flipped, 1: fliped by y-axis to handle mirror situation
+  auto lower = [&](int u, int v, int f) -> optional<int> {
+    for (auto e = h[u]->next[0]; e != h[u]; e = e->next[0]) {
+      auto w = e->to;
+      auto d = det(a[u] - a[v], a[w] - a[v]) * (!f - f);  // * -1 if flipped
+      if (d > 0 || d == 0 && (a[u] - a[v]).abs2() > (a[w] - a[v]).abs2())
         return w;
     }
     return nullopt;
@@ -72,99 +86,64 @@ int divconq(const vector<cplx> &a, const vector<int> &ord,
   int flg = 1;  // flg: 1 -> u updated, 0 -> v updated
   // guarantee neighbours of each vertex will be traversed at most twice
   while (true) {
-    if (flg) {                           // if u is updated
-      if (auto res = lowerl(u, v))       // try update u again
-        u = *res;                        // if new u founded, update u
-      else if (auto res = lowerr(u, v))  // else, check the other side
-        v = *res, flg = 0;               // if new v founded, update v
+    if (flg) {                             // if u is updated
+      if (auto res = lower(u, v, 0))       // try update u again
+        u = *res;                          // if new u founded, update u
+      else if (auto res = lower(v, u, 1))  // else, check the other side
+        v = *res, flg = 0;                 // if new v founded, update v
       else  // else, no newer u, v can be found, break
         break;
-    } else {                             // if v is updated
-      if (auto res = lowerr(u, v))       // try update v again
-        v = *res;                        // if new v founded, update v
-      else if (auto res = lowerl(u, v))  // else, check the other side
-        u = *res, flg = 1;               // if new u founded, update u
+    } else {                               // if v is updated
+      if (auto res = lower(v, u, 1))       // try update v again
+        v = *res;                          // if new v founded, update v
+      else if (auto res = lower(u, v, 0))  // else, check the other side
+        u = *res, flg = 1;                 // if new u founded, update u
       else  // else, no newer u, v can be found, break
         break;
     }
   }
   // insert base LR-edge, note that uv is the first edge of u and last of v
-  auto euv = e[u].insert(e[u].begin(), edge()),
-       evu = e[v].insert(e[v].end(), edge());
-  *euv = {v, evu}, *evu = {u, euv};
-  // next element, guarantee that it points to a element of a
-  auto nexte = [](list<edge> &a, list<edge>::iterator it) {
-    return ++it == a.end() ? a.begin() : it;
+  auto [uv, vu] = newedge(u, v);
+  insert(h[u], 0, uv);  // insert uv in the front of h[u]
+  insert(h[v], 1, vu);  // insert vu in the back of h[v]
+  // next candidate, flipped by y-axis if f = 1
+  auto candidate = [&](int u, int v, edge *uv, int f) {
+    auto e1 = next(uv, f);       // e1 is the candidate, next[1] if flipped
+    for (auto e2 = next(e1, f);  // e2 is the next candidate
+         det(a[v] - a[u], a[e1->to] - a[u]) * (!f - f) > 0 &&  // < 180 degree
+         inside(a[u], a[v], a[e1->to], a[e2->to]);  // e2 inside circle u,v,e1
+         e1 = e2, e2 = next(e1, f))                 // note that e2 maybe uv
+      erase(e1->rev), erase(e1);                    // erase e1
+    return det(a[v] - a[u], a[e1->to] - a[u]) * (!f - f) > 0 ? e1 : nullptr;
   };
-  // prev element, guarantee that it points to a element of a
-  auto preve = [](list<edge> &a, list<edge>::iterator it) {
-    return it == a.begin() ? --a.end() : --it;
-  };
-  // next left candidate
-  auto nextl = [&](int u, int v,
-                   list<edge>::iterator euv) -> optional<list<edge>::iterator> {
-    auto p = nexte(e[u], euv);
-    for (auto q = nexte(e[u], p); sgn(det(a[v] - a[u], a[p->v] - a[u])) > 0 &&
-                                  inside(a[u], a[v], a[p->v], a[q->v]);
-         p = q, q = nexte(e[u], p)) {  // while q is in the circle uvp
-      int w = p->v;
-      e[w].erase(p->rev), e[u].erase(p);  // erase edge p
-    }                                     // note that q may be euv
-    return sgn(det(a[v] - a[u], a[p->v] - a[u])) > 0 ? optional(p) : nullopt;
-  };
-  // next right candidate
-  auto nextr = [&](int u, int v,
-                   list<edge>::iterator evu) -> optional<list<edge>::iterator> {
-    auto p = preve(e[v], evu);
-    for (auto q = preve(e[v], p); sgn(det(a[u] - a[v], a[p->v] - a[v])) < 0 &&
-                                  inside(a[u], a[v], a[p->v], a[q->v]);
-         p = q, q = preve(e[v], p)) {  // same with nextl
-      int w = p->v;
-      e[w].erase(p->rev), e[v].erase(p);
-    }
-    return sgn(det(a[u] - a[v], a[p->v] - a[v])) < 0 ? optional(p) : nullopt;
-  };
-  auto updu = [&](int &u, int v, list<edge>::iterator &euv,
-                  list<edge>::iterator &evu, list<edge>::iterator &euu) {
-    int uu = euu->v;
-    auto ruu = euu->rev;
-    auto euuv = e[uu].insert(next(ruu), edge()),
-         evuu = e[v].insert(evu, edge());
-    u = uu, euv = euuv, evu = evuu;
-    *euv = {v, evu}, *evu = {u, euv};
-  };
-  auto updv = [&](int u, int &v, list<edge>::iterator &euv,
-                  list<edge>::iterator &evu, list<edge>::iterator &evv) {
-    int vv = evv->v;
-    auto rvv = evv->rev;
-    auto evvu = e[vv].insert(rvv, edge()),
-         euvv = e[u].insert(next(euv), edge());
-    v = vv, euv = euvv, evu = evvu;
-    *euv = {v, evu}, *evu = {u, euv};
+  // update next LR edge as wv, flipped by y-axis if f=1
+  auto updatelr = [&](int &u, int v, edge *&uv, edge *&vu, edge *uw, int f) {
+    int w = uw->to;
+    auto wu = uw->rev;
+    auto [wv, vw] = newedge(w, v);
+    insert(wu, f, wv);
+    insert(vu, !f, vw);
+    u = w, uv = wv, vu = vw;
   };
   while (true) {
-    auto euu = nextl(u, v, euv),
-         evv = nextr(u, v, evu);  // find potential candidate
-    if (!euu && !evv) break;      // none potential candidate found
-    if (!euu) {
-      updv(u, v, euv, evu, *evv);  // potential u found
-      continue;
-    }
-    if (!evv) {
-      updu(u, v, euv, evu, *euu);  // potential v found
-      continue;
-    }
-    // both found
-    if (inside(a[u], a[v], a[(*euu)->v], a[(*evv)->v]))
-      updv(u, v, euv, evu, *evv);  // v' inside uvu', choose v'
-    else
-      updu(u, v, euv, evu, *euu);  // v' not inside uvu', choose u'
+    auto uu2 = candidate(u, v, uv, 0),
+         vv2 = candidate(v, u, vu, 1);  // find potential candidate
+    if (!uu2 && !vv2)
+      break;  // no candidate found
+    else if (!vv2)
+      updatelr(u, v, uv, vu, uu2, 0);  // candidate u found
+    else if (!uu2)
+      updatelr(v, u, vu, uv, vv2, 1);  // candidate v found
+    else                               // both found
+      if (inside(a[u], a[v], a[uu2->to], a[vv2->to]))
+        updatelr(v, u, vu, uv, vv2, 1);  // v' inside uvu', choose v'
+      else
+        updatelr(u, v, uv, vu, uu2, 0);  // v' not inside uvu', choose u'
   }
   // uv is on the border of convex hull, be the last edge of u and first of v
-  e[u].erase(euv), e[v].erase(evu);
-  euv = e[u].insert(e[u].end(), edge());
-  evu = e[v].insert(e[v].begin(), edge());
-  *euv = {v, evu}, *evu = {u, euv};
+  erase(uv), erase(vu);
+  insert(h[u], 1, uv);
+  insert(h[v], 0, vu);
   return w;  // return low point of convex
 }
 // find the deuaunay tranagulation of a vertex set with no repeat nodes
@@ -175,10 +154,12 @@ vector<vector<int>> delaunay(const vector<cplx> &a) {
   vector<int> ord(n);
   iota(ord.begin(), ord.end(), 0);
   sort(ord.begin(), ord.end(), [&](int i, int j) { return a[i] < a[j]; });
-  vector<list<edge>> e(n);
-  divconq(a, ord, e, 0, n);
+  vector<edge *> h(n);
+  for (auto &e : h) e = new edge(-1), e->next = {e, e};
+  divconq(a, ord, h, 0, n);
   vector<vector<int>> res(n);
   for (int i = 0; i < n; i++)
-    for (auto &[v, r] : e[i]) res[i].push_back(v);
+    for (auto e = h[i]->next[0]; e != h[i]; e = e->next[0])
+      res[i].push_back(e->to);
   return res;
 }
