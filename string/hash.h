a36c54aff246fc2aff6f7c1f089153304a8ed0ac
Kilo-5723 <yunxiang.zhao.stu@gmail.com>
2024-04-22 01:05:18 +0800

    upd

--- string/hash.h
+++ string/hash.h
@@ -3,11 +3,12 @@ using namespace std;
 typedef long long ll;
 random_device r;
 default_random_engine e(r());
-uniform_int_distribution<int> g(2, 9e8);
+uniform_int_distribution<int> g(2, 9e8);  // random engine for base
 struct hsh {
   static const int k = 2;
   array<ll, k> a;
 };
+// hash of a single character
 hsh init(int c) {
   hsh a;
   fill(a.a.begin(), a.a.end(), c);
@@ -32,18 +33,21 @@ bool operator!=(hsh a, hsh b) {
     if (a.a[i] != b.a[i]) return true;
   return false;
 }
+// power of base
 hsh pmul(int n) {
   static vector<hsh> res = {{1, 1}};
   while (res.size() <= n) res.push_back(mul * res.back());
   return res[n];
 }
+// hashing of string, supports query for hash of substring
 struct hshstr {
   vector<hsh> h;
   hshstr(string s = "") {
     h.push_back({0, 0});
     for (auto c : s) h.push_back(h.back() * mul + init(c));
   }
-  hsh hash(int l, int r) { return h[r] - h[l] * pmul(r - l); }  // [l,r)
+  // return hash of substring s_l ... s_r-1, 0-based
+  hsh hash(int l, int r) { return h[r] - h[l] * pmul(r - l); }
 };
 // verified by:
 // https://codeforces.com/contest/1951/problem/E
